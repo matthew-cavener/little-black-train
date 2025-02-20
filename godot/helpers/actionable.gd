9791ba2dd8054c78be703c15424ddf0ebd55c793
Matthew Cavener <matthew.cavener@gmail.com>
2025-02-19 21:43:19 -0800

    improve character switching

--- godot/helpers/actionable.gd
+++ godot/helpers/actionable.gd
@@ -3,5 +3,8 @@ extends Area2D
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
 
+@onready var parent = get_parent()
+
 func action() -> void:
+    print(parent)
     DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
--- godot/scenes/character.gd
+++ godot/scenes/character.gd
@@ -10,24 +10,53 @@ const JUMP_VELOCITY = -400.0
 @export var player_velocity_multiplier: float = 1.5
 
 @onready var actionable = $Actionable
+@onready var actionable_shape = $Actionable/ActionableShape
 @onready var actionable_finder = $ActionableFinder
+@onready var actionable_finder_shape = $ActionableFinder/ActionableFinderShape
+
+var is_in_dialogue: bool = false
 
 func _ready() -> void:
     DialogueManager.dialogue_started.connect(_on_dialogue_started)
     DialogueManager.dialogue_ended.connect(_on_dialogue_finished)
+    add_to_group(character_name)
+    if is_player_controlled:
+        actionable_shape.set_deferred("disabled", true)
+        actionable_finder_shape.set_deferred("disabled", false)
+    else:
+        actionable_shape.set_deferred("disabled", false)
+        actionable_finder_shape.set_deferred("disabled", true)
 
 func _physics_process(delta: float) -> void:
     if not is_on_floor():
         velocity += get_gravity() * delta
-    if is_player_controlled:
-        $Actionable/ActionableShape.set_deferred("disabled", true)
-        $ActionableFinder/ActionableFinderShape.set_deferred("disabled", false)
+    if is_player_controlled and not is_in_dialogue:
         handle_player_input()
-    else:
-        $Actionable/ActionableShape.set_deferred("disabled", false)
-        $ActionableFinder/ActionableFinderShape.set_deferred("disabled", true)
+    elif not is_player_controlled:
         return_to_resting_location()
     move_and_slide()
+
+func _on_dialogue_started(_resource) -> void:
+    is_in_dialogue = true
+
+func _on_dialogue_finished(_resource) -> void:
+    is_in_dialogue = false
+
+func switch_character(character_name: String) -> void:
+    var new_character = get_tree().get_nodes_in_group(character_name)[0]
+    if new_character == self:
+        print("Tried switching to current character")
+        return
+    is_player_controlled = false
+    remove_from_group("player")
+    actionable_shape.set_deferred("disabled", false)
+    actionable_finder_shape.set_deferred("disabled", true)
+
+    new_character.is_player_controlled = true
+    new_character.add_to_group("player")
+    new_character.actionable_shape.set_deferred("disabled", true)
+    new_character.actionable_finder_shape.set_deferred("disabled", false)
+
 
 func handle_player_input() -> void:
     if Input.is_action_just_pressed("ui_accept"):
@@ -45,11 +74,5 @@ func return_to_resting_location() -> void:
         position.x = resting_location.x
         velocity.x = 0
     else:
-        var direction: int = signi(resting_location.x - position.x)
+        var direction: float = signf(resting_location.x - position.x)
         velocity.x = direction * SPEED
-
-func _on_dialogue_finished():
-    set_process_input(true)
-
-func _on_dialogue_started():
-    set_process_input(false)
