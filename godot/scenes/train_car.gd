98fe4923169e891c9e1e40299be72af8fc745a6f
Matthew Cavener <matthew.cavener@gmail.com>
2025-02-23 12:57:23 -0800

    add train car art, interior transition

---
+++ godot/scenes/train_car.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    DialogueManager.dialogue_started.connect(_on_dialogue_started)
+    DialogueManager.dialogue_ended.connect(_on_dialogue_finished)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
+
+func _on_dialogue_started(_resource) -> void:
+    var tween = create_tween()
+    tween.tween_property($Exterior, "modulate:a", 0, 1)
+
+func _on_dialogue_finished(_resource) -> void:
+    var tween = create_tween()
+    tween.tween_property($Exterior, "modulate:a", 1, 1)
