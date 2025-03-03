a67107a5bd04e271cef61ba96a31fbb622b65073
rachel-cavener <rachel.k.cavener@gmail.com>
2025-03-03 15:38:05 -0800

    Set up dialogue
    set up the dialogue between Alice and Mr. Jones and Mr.Jones and Alice.

--- godot/helpers/actionable.gd
+++ godot/helpers/actionable.gd
@@ -10,4 +10,6 @@ extends Area2D
 func action(character: CharacterBody2D) -> void:
     player_character = get_tree().get_first_node_in_group("player")
     character_spoken_to = character
+    print("alice_and_mr_jones flag: %s" % State.alice_and_mrjones_had_first_conversation)
+    print("mr_jones_and_alice flag: %s" % State.mrjones_and_alice_had_first_conversation)
     DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start, [self])
--- godot/state.gd
+++ godot/state.gd
@@ -8,12 +8,21 @@ var topher_has_charles_ticket: bool = false
 var topher_has_mrjones_ticket: bool = false
 var topher_has_msliddell_ticket: bool = false
 var topher_has_tom_ticket: bool = false
-var topher_has_given_tickets: bool = false
+var topher_has_given_tickets: bool = true
 
+#Alice Conversations
 var alice_and_mrjones_had_first_conversation: bool = false
-var mrjones_and_alice_had_first_conversation:bool = false
-
+var mrjones_and_alice_had_first_conversation: bool = false
+var alice_and_mrjones_have_been_introduced: bool = false
 
 var alice_and_tom_had_first_conversation: bool = false
 var tom_and_alice_had_first_conversation: bool = false
-#var alice_to_msliddell_first_conversation: bool = false
+
+#var alice_and_msliddell_first_conversation: bool = false
+#var msliddell_and_alice_first_conversation:bool = false
+
+#var alice_and_charles_had_first_conversation: bool = false
+#var charles_and_alice_had_first_conversation: bool = false
+
+#var alice_and_topher_had_a_conversation: bool = false
+#var alice_has_conversed_with_other_passengers: bool = false
